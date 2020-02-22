e73c9a0cc3f5100fc3c0de96a68c9677fc41022d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-22 16:37:56 +0000

    Deploy tschinz/zawiki to github.com/tschinz/zawiki.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.11-15-g891d9d9',
+    VERSION: 'v0.11-16-g7b1747e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
