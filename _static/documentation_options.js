512a217db3e03c0acebc26ef7fc33c61b745bf96
tschinz <tschinz@users.noreply.github.com>
2021-04-21 10:59:25 +0000

    Deploying to gh-pages from  @ 4bdd9ce0866015a75af15a1c5887bc413c1f2f3d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '87e19d1',
+    VERSION: '4bdd9ce',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
